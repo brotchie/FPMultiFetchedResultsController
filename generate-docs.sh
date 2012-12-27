52ef68c7187497314d5c13583f27132c437090f0
James Brotchie <brotchie@gmail.com>
2012-12-27 23:32:25 +1000

    Added script to generate appledoc documentation.

---
+++ generate-docs.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+appledoc -h --verbose 4 --keep-undocumented-members --project-name FPMultiFetchedResultsController --project-company "Factorial Products" --company-id com.factorialproducts --output docs MultiResultsController/FPMultiFetchedResultsController.h
