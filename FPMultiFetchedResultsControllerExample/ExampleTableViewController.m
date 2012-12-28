351fcf749aacd0ede152e7f1acaddcbf7d1e8b6e
James Brotchie <brotchie@gmail.com>
2012-12-28 13:24:36 +1000

    Added MIT license to all files.

--- FPMultiFetchedResultsControllerExample/ExampleTableViewController.m
+++ FPMultiFetchedResultsControllerExample/ExampleTableViewController.m
@@ -1,10 +1,27 @@
-//
-//  ExampleTableViewController.m
-//  MultiResultsController
-//
-//  Created by James Brotchie on 27/12/12.
-//  Copyright (c) 2012 James Brotchie. All rights reserved.
-//
+/*
+ The MIT License
+ 
+ Copyright (c) 2012 James Brotchie <brotchie@gmail.com>
+ 
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the
+ "Software"), to deal in the Software without restriction, including
+ without limitation the rights to use, copy, modify, merge, publish,
+ distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to
+ the following conditions:
+ 
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 #import "ExampleTableViewController.h"
 #import "DataModel.h"
@@ -90,10 +107,6 @@
     [self.tableView beginUpdates];
 }
 
-- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
-    [self.tableView endUpdates];
-}
-
 - (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath {
     switch (type) {
         case NSFetchedResultsChangeInsert:
@@ -106,6 +119,7 @@
             [self.tableView moveRowAtIndexPath:indexPath toIndexPath:newIndexPath];
             break;
         case NSFetchedResultsChangeUpdate:
+            // Do nothing when non-sorting related properties change.
             break;
     }
 }
@@ -119,6 +133,10 @@
             [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationLeft];
             break;
     }
+}
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+    [self.tableView endUpdates];
 }
 
 @end
